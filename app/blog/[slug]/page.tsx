fe6530011858ca7f6a5e3179ce94430dc400f9c3
shailus1029 <shailus1029@gmail.com>
2024-08-13 19:34:59 +0530

    Accessing slug name

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,10 @@
-const BlogPostPage = () => {
+
+/* This params props is passed by nextjs */
+const BlogPostPage = ({ params }: any) => {
     return (
         <main>
             <h1>Blog Post</h1>
+            <p>{params.slug}</p>
         </main>
     )
 }
